567fd14bd991ba81a08d03b49004470fb04c22f8
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-10-01 21:21:36 +0530

    Update web.py

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 import webbrowser
 st.title("Pypack")
 image_url = "https://raw.githubusercontent.com/ArsanCodifire/Pypack/refs/heads/main/bg.png"
-
+auth_url="https://discord.com/oauth2/authorize?client_id=1290358916886298769&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fpypack.streamlit.app&integration_type=0&scope=identify+bot"
 # CSS to set the background image
 st.markdown(
     f"""
@@ -19,3 +19,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+def log():
+    webbrowser.open_new_tab(auth_url)
+clk=st.button("Authorise",on_click=log())
